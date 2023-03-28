fece9ab2f3df72e0b58faf89450716f5890e78a9
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-28 14:40:53 -0700

    Revert "adjustments to variables"
    This reverts commit f49b0ca9e06299baa72546ccad5419df6d6854ce.

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
 
 /**
  * _printf - formatted output conversion and print data.
@@ -14,8 +13,7 @@ int _printf(const char *format, ...)
 	int (*func)(va_list, char *, unsigned int);
 	char *buffer;
 
-	va_start(args, format);
-	buffer = malloc(sizeof(char) * 1024);
+	va_start(args, format), buffer = malloc(sizeof(char) * 1024);
 	if (!format || !buffer || (format[n] == '%' && !format[n + 1]))
 		return (-1);
 	if (!format[n])
