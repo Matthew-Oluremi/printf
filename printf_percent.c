6c73a998800a78707e4230bbc116420339d1fda8
Matthew Oluremi <matthewoluremi376@gmail.com>
2023-03-28 15:08:36 -0700

    code tested

--- printf_percent.c
+++ printf_percent.c
@@ -5,12 +5,12 @@
  * print_prg - writes the character c to stdout
  * @a: input char
  * @buff: buffer pointer
- * @n: index for buffer pointer
+ * @i: index for buffer pointer
  * Return: On success 1.
  */
-int print_prg(va_list a __attribute__((unused)), char *buff, unsigned int n)
+int print_prg(va_list a __attribute__((unused)), char *buff, unsigned int i)
 {
-	handl_buf(buff, '%', n);
+	handl_buf(buff, '%', i);
 
 	return (1);
 }
